129ff6abe01f4f710d2cd451e6c1b25409fbaae7
wwx <wwx@wlaa.com.cn>
2021-04-20 10:27:41 +0800

    完善连线机制

--- src/main/java/sample/controller/NewShipFileController.java
+++ src/main/java/sample/controller/NewShipFileController.java
@@ -9,16 +9,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.CacheHint;
 import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -34,6 +29,7 @@ import sample.common.PublicConfigure;
 import sample.common.WhileButton;
 import sample.entity.ship.ShipJson;
 import sample.javaFXUI.NodePane;
+import sample.javaFXUI.NodePaneTransparent;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -76,9 +72,14 @@ public class NewShipFileController {
     private Button ButBottom;
     @FXML
     private ComboBox comboBox;
+    @FXML
+    private CheckBox checkBoxImage;
 
     @FXML
     private TextArea testAreatest;
+
+    private static double mouseX=0;
+    private static double mouseY=0;
 
     ObservableList<comboBoxEnt> options= FXCollections.observableArrayList();
     List<NodePane> weapons=new ArrayList<>();
@@ -116,32 +117,22 @@ public class NewShipFileController {
             imageView.setFitWidth(w);
             imageView.setImage(image);
 
-            pane.heightProperty().addListener(new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                    imageView.setLayoutY((newValue.doubleValue() - imageView.getFitHeight()) / 2);
-                    coreImage.setLayoutY((newValue.doubleValue() - coreImage.getFitHeight()) / 2);
 
-                }
+            pane.heightProperty().addListener((observable, oldValue, newValue) -> {
+                imageView.setLayoutY((newValue.doubleValue() - imageView.getFitHeight()) / 2);
+                coreImage.setLayoutY((newValue.doubleValue() - coreImage.getFitHeight()) / 2);
             });
-            pane.widthProperty().addListener(new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                    imageView.setLayoutX((newValue.doubleValue() - imageView.getFitWidth()) / 2);
-                    coreImage.setLayoutX((newValue.doubleValue() - coreImage.getFitWidth()) / 2);
-
-                }
+            pane.widthProperty().addListener((observable, oldValue, newValue) -> {
+                imageView.setLayoutX((newValue.doubleValue() - imageView.getFitWidth()) / 2);
+                coreImage.setLayoutX(((newValue.doubleValue() - coreImage.getFitWidth()) / 2));
             });
 
             lable2.setText("中心=" + h / 2 + "--" + w / 2);
             lable3.setText("长" + h + "宽" + w);
 
-
             coreImage.setImage(new Image("image/core.png"));
-            coreImage.setFitHeight(100);
-            coreImage.setFitWidth(100);
-
-
+            coreImage.setFitHeight(50);
+            coreImage.setFitWidth(50);
 
             TranslateTransition translateTransition = new TranslateTransition(Duration.millis(1), pane);
             translateTransition.setToX(0);
@@ -157,14 +148,7 @@ public class NewShipFileController {
             });
 
             pane.setOnMouseMoved(event -> {
-                double vw = (imageView.getFitWidth() - w) / 2;
-                double vh = (imageView.getFitHeight() - h) / 2;
-                double layoutX = coreImage.getLayoutX() + (coreImage.getFitWidth() / 2);
-                double layoutY = coreImage.getLayoutY() + (coreImage.getFitHeight() / 2);
-
-                lable1.setText(RelativeCoordinates_plus(layoutY , event.getY() , vh) + "--" +
-                        RelativeCoordinates_plus(layoutX , event.getX() , vw)+"\n"
-                +event.getY()+"--"+event.getX());
+                MouseMoved(event);
             });
             pane.setOnScroll(event -> {
                 double toX = scaleTransition.getToX();
@@ -182,29 +166,68 @@ public class NewShipFileController {
             });
 
             supPane.setOnKeyPressed(eventKEY -> {
+                double layoutX = coreImage.getLayoutX() + (coreImage.getFitWidth() / 2);
+                double layoutY = coreImage.getLayoutY() + (coreImage.getFitHeight() / 2);
                 if (eventKEY.getCode()== KeyCode.CONTROL){
+                    NodePaneTransparent node=new NodePaneTransparent(mouseX,mouseY);
+                    node.setId("NodePane1");
+                    node.setRX(RelativeCoordinates_plus(layoutX,mouseX));
+                    node.setRY(RelativeCoordinates_plus(layoutY,mouseY));
+                    NodePaneTransparent node2=new NodePaneTransparent(RelativeCoordinates_minus(layoutX,node.getRX()>0? 0-node.getRX():Math.abs(node.getRX())),mouseY);
+                    node2.setId("NodePane2");
+                    if (checkBoxImage.isSelected()){
+                        pane.getChildren().addAll(node,node2);
+                        TransparentLine(node,node2);
+                    }else{
+                        pane.getChildren().addAll(node);
+                        TransparentLine(node);
+                    }
+
+                    pane.setOnMouseMoved(event -> {
+                        MouseMoved(event);
+                        node.relocate(mouseX-10,mouseY-10);
+                        node.setRX(RelativeCoordinates_plus(layoutX,mouseX));
+                        if (checkBoxImage.isSelected()){
+                            double v = mouseX - 10;
+                            node2.relocate(RelativeCoordinates_minus(layoutX,node.getRX()>0? 0-node.getRX():Math.abs(node.getRX()))-10,mouseY-10);
+                        }
+
+                    });
+
+
                     pane.setOnMouseClicked(event -> {
-                        double vw = (imageView.getFitWidth() - w) / 2;
-                        double vh = (imageView.getFitHeight() - h) / 2;
-                        double layoutX = coreImage.getLayoutX() + (coreImage.getFitWidth() / 2);
-                        double layoutY = coreImage.getLayoutY() + (coreImage.getFitHeight() / 2);
                         NodePane nodePane=new NodePane(event.getX(),event.getY());
-                        nodePane.setRX(RelativeCoordinates_plus(layoutX , event.getX() , vw));
-                        nodePane.setRY(RelativeCoordinates_plus(layoutY , event.getY() , vh));
+                        nodePane.setRX(RelativeCoordinates_plus(layoutX , event.getX()));
+                        nodePane.setRY(RelativeCoordinates_plus(layoutY , event.getY()));
                         nodePane.setCode(weapons.size());
                         weapons.add(nodePane);
                         pane.getChildren().addAll(nodePane);
                         Connection(nodePane);
                         draggable(nodePane);
+                        if (checkBoxImage.isSelected()){
+                            TransparentLine(node,node2);
+                        }else{
+                            TransparentLine(node);
+                        }
                     });
                 }
             });
 
             supPane.setOnKeyReleased(eventKEY -> {
                 if (eventKEY.getCode()== KeyCode.CONTROL){
-                       pane.setOnMouseClicked(event -> {
+                    pane.setOnMouseClicked(event -> {
 
-                       });
+                    });
+                    pane.setOnMouseMoved(event -> {
+                        MouseMoved(event);
+                    });
+                    pane.getChildren().removeIf(n ->{
+                        if (StrUtil.equals("NodePaneTransparent",n.getTypeSelector())){
+                            if (StrUtil.indexOfIgnoreCase(n.getId(),"NodePane")>=0)return true;
+                        }else
+                        if (StrUtil.indexOfIgnoreCase(n.getId(),"LineA")>=0)return true;
+                        return false;
+                            });
                 }
             });
 
@@ -236,11 +259,59 @@ public class NewShipFileController {
             });
             new WhileButton(ButRight, ButBottom, ButTop, ButLeft);
             comboBox.setItems(options);
-
         }
 
     }
-    private void draggable(Node node) {
+
+    public void TransparentLine(NodePaneTransparent node){
+        pane.getChildren().removeIf(n -> StrUtil.indexOfIgnoreCase(n.getId(),"LineA")>=0);
+        if (weapons.size()!=0){
+            Line line1 = TheConnection(node, weapons.get(0));
+            line1.setId("LineA1");
+            line1.setOpacity(0.3);
+            line1.setStroke(Paint.valueOf("#09f60e"));
+            line1.setStrokeWidth(3);
+            Line line2 = TheConnection(node, weapons.get(weapons.size() - 1));
+            line2.setId("LineA2");
+            line2.setOpacity(0.3);
+            line2.setStroke(Paint.valueOf("#09f60e"));
+            line2.setStrokeWidth(3);
+            pane.getChildren().addAll(line1, line2);
+        }
+    }
+
+    public void TransparentLine(NodePaneTransparent node1,NodePaneTransparent node2){
+        pane.getChildren().removeIf(n -> StrUtil.indexOfIgnoreCase(n.getId(),"LineA")>=0);
+        if (weapons.size()!=0){
+            Line line1 = TheConnection(node1, weapons.get(0));
+            line1.setId("LineA1");
+            line1.setOpacity(0.3);
+            line1.setStroke(Paint.valueOf("#09f60e"));
+            line1.setStrokeWidth(3);
+            Line line2 = TheConnection(node1, node2);
+            line2.setId("LineA1");
+            line2.setOpacity(0.3);
+            line2.setStroke(Paint.valueOf("#09f60e"));
+            line2.setStrokeWidth(3);
+            Line line3 = TheConnection(node2, weapons.get(weapons.size() - 1));
+            line3.setId("LineA3");
+            line3.setOpacity(0.3);
+            line3.setStroke(Paint.valueOf("#09f60e"));
+            line3.setStrokeWidth(3);
+            pane.getChildren().addAll(line1, line2,line3);
+        }
+    }
+
+    public void MouseMoved(MouseEvent event){
+        mouseX=event.getX();
+        mouseY=event.getY();
+        double layoutX = coreImage.getLayoutX() + (coreImage.getFitWidth() / 2);
+        double layoutY = coreImage.getLayoutY() + (coreImage.getFitHeight() / 2);
+        lable1.setText(RelativeCoordinates_plus(layoutY, event.getY()) + "==" +
+                RelativeCoordinates_plus(layoutX, event.getX()));
+    }
+
+    private void draggable(NodePane node) {
         final Position pos = new Position();
         // 提示用户该结点可点击
         node.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> node.setCursor(Cursor.HAND));
@@ -261,6 +332,16 @@ public class NewShipFileController {
             // 计算出 x、y 后将结点重定位到指定坐标点 (x, y)
             node.relocate(x, y);
         });
+        node.addEventHandler(MouseEvent.MOUSE_RELEASED,event -> {
+            double layoutX = coreImage.getLayoutX() + (coreImage.getFitWidth() / 2);
+            double layoutY = coreImage.getLayoutY() + (coreImage.getFitHeight() / 2);
+            double distanceX = event.getX() - pos.x;
+            double distanceY = event.getY() - pos.y;
+            double x = node.getLayoutX() + distanceX;
+            double y = node.getLayoutY() + distanceY;
+            node.setRX(RelativeCoordinates_plus(layoutX,x+10));
+            node.setRY(RelativeCoordinates_plus(layoutY,y+10));
+        });
     }
 
     private static class Position {
@@ -279,11 +360,11 @@ public class NewShipFileController {
         prog.setProgress(0.0);
     }
 
-    public double RelativeCoordinates_plus(double layout,double event,double v){
-        return NumberUtil.round(layout - event - v,1).doubleValue();
+    public double RelativeCoordinates_plus(double layout,double event){
+        return NumberUtil.round(layout - event ,1).doubleValue();
     }
 
-    public double RelativeCoordinates_minus(double layout,double event,double v ){
+    public double RelativeCoordinates_minus(double layout,double event ){
         double v1=0;
         if (event>0){
              v1 = layout - event;
@@ -294,23 +375,30 @@ public class NewShipFileController {
         return NumberUtil.round(v1,1).doubleValue();
     }
 
-    public void Connection(NodePane nodePane){
-        pane.getChildren().removeIf(node -> StrUtil.equals(node.getTypeSelector(),"Line"));
+
+
+
+
+    public void Connection(NodePane nodePane) {
+        pane.getChildren().removeIf(node -> StrUtil.equals(node.getTypeSelector(), "Line")
+                && StrUtil.indexOfIgnoreCase(node.getId(), "LineA") < 0);
         int code = nodePane.getCode();
-        if (code>0){
-          for (NodePane np:weapons){
-              if (np.getCode()<weapons.size()-1){
-                  pane.getChildren().addAll(TheConnection(np,weapons.get(np.getCode()+1)));
-              }else {
-                  pane.getChildren().add(TheConnection(np,weapons.get(0)));
-              }
-          }
+        if (code > 0) {
+            for (NodePane np : weapons) {
+                if (np.getCode() < weapons.size() - 1) {
+                    pane.getChildren().addAll(TheConnection(np, weapons.get(np.getCode() + 1)));
+                } else {
+                    pane.getChildren().add(TheConnection(np, weapons.get(0)));
+                }
+            }
         }
     }
 
+
     public Line TheConnection(Pane node1, Pane node2){
         Line line = new Line();
-
+        line.setStrokeWidth(3);
+        line.setOpacity(0.5);
         // 将直线的起点坐标与 node1 的中心坐标进行绑定
         line.startXProperty().bind(node1.layoutXProperty().add(node1.widthProperty().divide(2)));
         line.startYProperty().bind(node1.layoutYProperty().add(node1.heightProperty().divide(2)));
@@ -346,4 +434,6 @@ public class NewShipFileController {
             this.val = val;
         }
     }
+
+
 }
